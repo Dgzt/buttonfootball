a8bddb47eaf03d5bc5a3b6d541ee4f45caa42b14
Zsuró Tibor <zsurotibor@gmail.com>
2014-05-19 20:16:11 +0200

    Show tutorial opengl.

--- main.cpp
+++ main.cpp
@@ -23,36 +23,76 @@
 */
 #include <iostream>
 #include <SDL.h>
-#include <SDL_opengl.h>
+#include <GLES3/gl3.h>
+
+void
+sdldie( std::string msg ){
+    std::cout << msg << ": " << SDL_GetError() << std::endl;
+    SDL_Quit();
+    exit(1);
+}
 
 /*!
  * The main function.
  */
 int 
 main(){
-  
-  //Initialize the SDL library.
-  SDL_Init(SDL_INIT_VIDEO);
-  
-  //Setup the video mode.
-  SDL_Surface *screen = SDL_SetVideoMode(800, 600, 32, SDL_OPENGL);
 
-  //if (SDL_MUSTLOCK( screen ) ){ 
-  //  SDL_LockSurface(screen);
-  //}
-  
-  //OpenGL info.
-  //std::cout << "Renderer: " << glGetString (GL_RENDERER) << std::endl;
-  //std::cout << "Version: " << glGetString (GL_VERSION) << std::endl;
+    SDL_Window *mainwindow; /* Our window handle */
+    SDL_GLContext maincontext; /* Our opengl context handle */
 
-  //if ( SDL_MUSTLOCK( screen ) ){ 
-  //  SDL_UnlockSurface(screen);
-  //}
+    // Initialize SDL's Video subsystem
+    if( SDL_Init(SDL_INIT_VIDEO) < 0 ){
+        sdldie("Unable to initialize SDL");
+    }
   
-  //Swaps screen buffers.
-  //SDL_Flip(screen);
+    // Request opengl 3.2 context.
+    // SDL doesn't have the ability to choose which profile at this time of writing,
+    // but it should default to the core profile
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
 
-  //SDL_Quit();
-  
-  return 0;
+    // Turn on double buffering with a 24bit Z buffer.
+    // You may need to change this to 16 or 32 for your system
+    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+    SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+
+    // Create our window centered at 512x512 resolution
+    mainwindow = SDL_CreateWindow("PROGRAM_NAME", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 512, 512, SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN);
+    if (!mainwindow){
+        sdldie("Unable to create window");
+    }
+
+    // Create our opengl context and attach it to our window
+    maincontext = SDL_GL_CreateContext(mainwindow);
+
+    // This makes our buffer swap syncronized with the monitor's vertical refresh
+    SDL_GL_SetSwapInterval(1);
+
+    // Clear our buffer with a red background
+    glClearColor ( 1.0, 0.0, 0.0, 1.0 );
+    glClear ( GL_COLOR_BUFFER_BIT );
+    // Swap our back buffer to the front
+    SDL_GL_SwapWindow(mainwindow);
+    // Wait 2 seconds
+    SDL_Delay(2000);
+
+    // Same as above, but green
+    glClearColor ( 0.0, 1.0, 0.0, 1.0 );
+    glClear ( GL_COLOR_BUFFER_BIT );
+    SDL_GL_SwapWindow(mainwindow);
+    SDL_Delay(2000);
+
+    // Same as above, but blue
+    glClearColor ( 0.0, 0.0, 1.0, 1.0 );
+    glClear ( GL_COLOR_BUFFER_BIT );
+    SDL_GL_SwapWindow(mainwindow);
+    SDL_Delay(2000);
+
+    // Delete our opengl context, destroy our window, and shutdown SDL
+    SDL_GL_DeleteContext(maincontext);
+    SDL_DestroyWindow(mainwindow);
+    SDL_Quit();
+
+    return 0;
 }
