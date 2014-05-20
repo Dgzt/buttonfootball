0f0b1ff814889cf07ca109d5f32bc0c2886bfa16
Zsuró Tibor <zsurotibor@gmail.com>
2014-05-20 11:34:09 +0200

    Remove opengl 3 checking.

--- main.cpp
+++ main.cpp
@@ -45,12 +45,6 @@ main(){
     if( SDL_Init(SDL_INIT_VIDEO) < 0 ){
         sdldie("Unable to initialize SDL");
     }
-  
-    // Request opengl 3.2 context.
-    // SDL doesn't have the ability to choose which profile at this time of writing,
-    // but it should default to the core profile
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
 
     // Turn on double buffering with a 24bit Z buffer.
     // You may need to change this to 16 or 32 for your system
